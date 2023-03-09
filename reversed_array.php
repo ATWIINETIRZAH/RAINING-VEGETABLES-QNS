51f7152f8217feb2063259016784aa8b2aa15822
ATWIINETIRZAH <117368965+ATWIINETIRZAH@users.noreply.github.com>
2023-03-09 15:06:19 +0300

    Add files via upload
    we use the array_reverse function

---
+++ reversed_array.php
@@ -0,0 +1,7 @@
+$original_array = [56,89,34,9,63,57,16,2,64,72];
+
+// we use the array_reverse function to reverse the elements in the array
+$reversed_array= array_reverse($original_array);
+
+// we use the print_r function to display the reversed array
+print_r($reversed_array); 
