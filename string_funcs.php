bf0dc93d1e5192d59a2b935f20158cece5a21d6b
ATWIINETIRZAH <117368965+ATWIINETIRZAH@users.noreply.github.com>
2023-03-14 15:38:58 +0300

    Add files via upload
    use of string functions like strlen, strpos among others

---
+++ string_funcs.php
@@ -0,0 +1,14 @@
+<?php
+//outputs the number of characters counting the spaces as characters
+echo strlen("i am learning php"); 
+
+//outputs number of words
+echo str_word_count("there are seven words in this statement");
+
+//searches for specific text in a string
+echo strpos("Hello world", "world");
+
+//replaces text in a string with text in another string
+echo str_replace("book", "paper", "pens");
+
+?>
