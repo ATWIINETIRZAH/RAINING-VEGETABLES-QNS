405e4983b4e042739685189d6acf620de84ddc99
ATWIINETIRZAH <117368965+ATWIINETIRZAH@users.noreply.github.com>
2023-03-14 15:55:37 +0300

    Add files via upload

---
+++ multi_dimensional_array.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$family = 
+//an example of a multi dimensional array
+  array(
+    array("first_name" => "Tirzah", "age" => 20, "last_name" => "Atwiine"),
+    array("first_name" => "Ben", "age" => 25, "last_name" => "Kimmy"),
+    array("first_name" => "Tom", "age" => 57, "last_name" => "Bing")   
+  );
+  print_r($family)
+
+?>
